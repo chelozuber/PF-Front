cbb2d43a2ed4e19361ee2b2a40202f4daaf403dc
chelozuber <marcelozuber@hotmail.com>
2024-04-18 14:00:14 -0300

    armado del searchBar, solo la logica

---
+++ src/components/Search/searchBar.jsx
@@ -0,0 +1,39 @@
+import React, {useState} from 'react';
+import { useDispatch } from 'react-redux';
+import { searchProductsByBrand, searchProductsByName } from '../../redux/actions';
+
+const searchBar = () => {
+    const dispatch = useDispatch();
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchByBrand, setSearchByBrand] = useState(false);
+
+    const handleSearch = () => {
+        if (searchByBrand) {
+            dispatch(searchProductsByBrand(searchTerm));
+        } else {
+            dispatch(searchProductsByName(searchTerm));
+        }
+    };
+
+    return (
+        <div>
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder="Search..."
+          />
+          <button onClick={handleSearch}>Search</button>
+          <label>
+            <input
+              type="checkbox"
+              checked={searchByBrand}
+              onChange={() => setSearchByBrand(!searchByBrand)}
+            />
+            Search by Brand
+          </label>
+        </div>
+      );
+};
+
+export default searchBar; 
